948b57c7b31f0f3303dd4e4e99ef52f7d5446bff
JeanBilliardVega <163461785+JeanBilliardVega@users.noreply.github.com>
2024-04-25 21:25:05 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -38,15 +38,69 @@ int is_equal(void* key1, void* key2){
     return 0;
 }
 
+int get_valid_location(HashMap* map, char* key)
+{
+  int start_loc = hash(key, map->capacity);
+  if(map->buckets[start_loc]->key == key || map->buckets[start_loc] == NULL)
+  {
+    return start_loc;
+  }
+  else
+  {
+    int loc;
+    for(loc = start_loc; loc < map->capacity; loc++)
+    {
+      if(map->buckets[loc] == NULL || map->buckets[loc]->key == key)
+      {
+        return loc;
+      }
+    }
+    for(loc = 0; loc < start_loc; loc++)
+    {
+      if(map->buckets[loc] == NULL || map->buckets[loc]->key ==  || map->buckets[loc]->key == key)
+        return loc;
+    }
+  }
+  return -1;
+}
 
 void insertMap(HashMap * map, char * key, void * value) {
-
-
+  int loc = get_valid_location(map, key);
+  if(loc == -1)
+  {
+    enlarge(map);
+    insertMap(map, key, value);
+  }
+  float load_factor = (float)map->size / map->capacity;
+  if (load_factor > 0.7)
+  {
+    enlarge(map);
+    insertMap(map, key, value);
+  }
+  else
+  {
+    map->buckets[loc]->value = value;
+    map->buckets[loc]->key = key;
+  }  
 }
 
 void enlarge(HashMap * map) {
   enlarge_called = 1; //no borrar (testing purposes)
-  
+  int old_capacity = map->capacity;
+  Pair** old_array = map->buckets;
+  map->capacity = 2 * old_capacity;
+  map->buckets = (Pair**)calloc(map->capacity, sizeof(Pair*));
+  map->size = 0;
+
+  for (int i = 0; i < old_capacity; i++)
+  {
+    if (old_array[i] != NULL)
+    {
+      insertMap(map, old_array[i]->key, old_array[i]->value);
+      free(old_array[i]);
+    }
+  }
+  free(old_array);
 }
 
 
