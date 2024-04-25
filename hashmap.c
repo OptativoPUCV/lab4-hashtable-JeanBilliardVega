505a99fe8a064203bc364f5652b85b74dd7f8725
JeanBilliardVega <163461785+JeanBilliardVega@users.noreply.github.com>
2024-04-25 22:20:49 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -110,7 +110,15 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map)
-{
+{ 
+  for(int i = 0; i < map->capacity; i++)
+    {
+      if(map->buckets[i]->key != NULL && map->buckets[i] != NULL)
+      {
+        map->current = i;
+        return map->buckets[i];
+      }
+    }
   return NULL;
 }
 
