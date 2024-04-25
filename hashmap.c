efde6e5f44a4b5364d563e48d4f53a5709b64ec6
JeanBilliardVega <163461785+JeanBilliardVega@users.noreply.github.com>
2024-04-25 21:16:12 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,7 +53,7 @@ void enlarge(HashMap * map) {
 HashMap * createMap(long capacity) {
   HashMap* mapa = (HashMap*) malloc(sizeof(HashMap));
   mapa->capacity = capacity;
-  mapa->buckets = (Pair**) calloc(mapa->capacity,sizeof(Pair*));
+  mapa->buckets = (Pair**) calloc(capacity,sizeof(Pair*));
   mapa->size = 0;
   return mapa;
 }
