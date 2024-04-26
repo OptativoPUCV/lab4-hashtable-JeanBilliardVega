b3129a33c3dc2330e8a1dc1e0ca21453b5b64133
JeanBilliardVega <163461785+JeanBilliardVega@users.noreply.github.com>
2024-04-26 22:07:17 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -123,11 +123,12 @@ Pair * firstMap(HashMap * map)
 }
 
 Pair * nextMap(HashMap * map) {
-  if(map->current == -1) return NULL;
-  else
+  for(long i = map->current + 1; i < map->capacity; i++)
   {
-    long loc = map->current;
-    return map->buckets[(loc + 1)]->value;
+    if(map->buckets[i] != NULL && map->buckets[i]->key != NULL)
+    {
+      map->current = i;
+      return map->buckets[i];
+    }
   }
-  return NULL;
 }
